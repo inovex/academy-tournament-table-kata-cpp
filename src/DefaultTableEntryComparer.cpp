4659c29e340e74dd2d9e4e4feb36232280b158ce
Christoph Menzel <christoph.menzel@inovex.de>
2023-07-26 15:52:15 +0200

    Possible solution for exercise 5
    Extended the default table entry comparer and implemented unit tests
    for the new feature.

--- src/DefaultTableEntryComparer.cpp
+++ src/DefaultTableEntryComparer.cpp
@@ -4,5 +4,25 @@ bool DefaultTableEntryComparer::greaterThan(
     const ITableEntry &leftTableEntry,
     const ITableEntry &rightTableEntry) const
 {
+  if (leftTableEntry.getPoints() == rightTableEntry.getPoints())
+  {
+    int leftGoalDifference = CalculateGoalDifference(leftTableEntry);
+    int rightGoalDifference = CalculateGoalDifference(rightTableEntry);
+
+    if (leftGoalDifference == rightGoalDifference)
+    {
+      return leftTableEntry.getGoalsScored() > rightTableEntry.getGoalsScored();
+    }
+
+    return leftGoalDifference > rightGoalDifference;
+  }
+
   return leftTableEntry.getPoints() > rightTableEntry.getPoints();
+  return leftTableEntry.getPoints() > rightTableEntry.getPoints();
+}
+
+int DefaultTableEntryComparer::CalculateGoalDifference(
+    const ITableEntry &tableEntry) const
+{
+  return tableEntry.getGoalsScored() - tableEntry.getGoalsAgainst();
 }
