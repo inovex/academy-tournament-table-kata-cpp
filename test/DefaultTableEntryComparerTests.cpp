05924b22b201d62f3f876f3938002e9e9de17211
Christoph Menzel <christoph.menzel@inovex.de>
2023-07-26 15:52:15 +0200

    Possible solution for exercise 5
    Extended the default table entry comparer and implemented unit tests
    for the new feature.

--- test/DefaultTableEntryComparerTests.cpp
+++ test/DefaultTableEntryComparerTests.cpp
@@ -1,19 +1,22 @@
 #include "DefaultTableEntryComparer.h"
 #include "gtest/gtest.h"
 
-class DefaultTableEntryComparerTests : public ::testing::Test {
+class DefaultTableEntryComparerTests : public ::testing::Test
+{
 protected:
   std::shared_ptr<ITableEntryComparer> tableEntryComparer;
   std::shared_ptr<ITableEntry> germanyTableEntry;
   std::shared_ptr<ITableEntry> englandTableEntry;
 
-  virtual void SetUp() {
+  virtual void SetUp()
+  {
     tableEntryComparer = std::make_shared<DefaultTableEntryComparer>();
 
     germanyTableEntry = std::make_shared<TableEntry>();
     englandTableEntry = std::make_shared<TableEntry>();
   }
-  virtual void TearDown() {
+  virtual void TearDown()
+  {
     tableEntryComparer.reset();
 
     germanyTableEntry.reset();
@@ -22,7 +25,8 @@ protected:
 };
 
 TEST_F(DefaultTableEntryComparerTests,
-       greaterThan_BothTeamsHaveSamePoints_RetrunsFalse) {
+       greaterThan_BothTeamsHaveSamePoints_RetrunsFalse)
+{
   germanyTableEntry->addPoints(1);
   englandTableEntry->addPoints(1);
 
@@ -33,7 +37,8 @@ TEST_F(DefaultTableEntryComparerTests,
 }
 
 TEST_F(DefaultTableEntryComparerTests,
-       greaterThan_FirstTeamHasMorePoints_RetrunsFalse) {
+       greaterThan_FirstTeamHasMorePoints_RetrunsFalse)
+{
   germanyTableEntry->addPoints(2);
   englandTableEntry->addPoints(1);
 
@@ -44,7 +49,8 @@ TEST_F(DefaultTableEntryComparerTests,
 }
 
 TEST_F(DefaultTableEntryComparerTests,
-       greaterThan_FirstTeamHasLessPoints_RetrunsFalse) {
+       greaterThan_FirstTeamHasLessPoints_RetrunsFalse)
+{
   germanyTableEntry->addPoints(1);
   englandTableEntry->addPoints(2);
 
@@ -52,4 +58,82 @@ TEST_F(DefaultTableEntryComparerTests,
       tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
 
   ASSERT_FALSE(result);
+}
+TEST_F(
+    DefaultTableEntryComparerTests,
+    greaterThan_BothTeamsHaveSamePointsAndFirstTeamLessGoalDifference_RetrunsFalse)
+{
+  germanyTableEntry->addPoints(1);
+  englandTableEntry->addPoints(1);
+  germanyTableEntry->addGoalsScored(1);
+  englandTableEntry->addGoalsScored(2);
+
+  bool result =
+      tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
+
+  ASSERT_FALSE(result);
+}
+
+TEST_F(
+    DefaultTableEntryComparerTests,
+    greaterThan_BothTeamsHaveSamePointsAndFirstTeamGreaterGoalDifference_RetrunsFalse)
+{
+  germanyTableEntry->addPoints(1);
+  englandTableEntry->addPoints(1);
+  germanyTableEntry->addGoalsScored(2);
+  englandTableEntry->addGoalsScored(1);
+
+  bool result =
+      tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
+
+  ASSERT_TRUE(result);
+}
+
+TEST_F(
+    DefaultTableEntryComparerTests,
+    greaterThan_BothTeamsHaveSamePointsAndSameGoalDifferenceAndSameScoredGoals_RetrunsFalse)
+{
+  germanyTableEntry->addPoints(1);
+  englandTableEntry->addPoints(1);
+  germanyTableEntry->addGoalsScored(1);
+  englandTableEntry->addGoalsScored(1);
+
+  bool result =
+      tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
+
+  ASSERT_FALSE(result);
+}
+
+TEST_F(
+    DefaultTableEntryComparerTests,
+    greaterThan_BothTeamsHaveSamePointsAndSameGoalDifferenceAndFirstTeamLessScoredGoals_RetrunsFalse)
+{
+  germanyTableEntry->addPoints(1);
+  englandTableEntry->addPoints(1);
+  germanyTableEntry->addGoalsScored(1);
+  germanyTableEntry->addGoalsAgainst(2);
+  englandTableEntry->addGoalsScored(2);
+  englandTableEntry->addGoalsAgainst(3);
+
+  bool result =
+      tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
+
+  ASSERT_FALSE(result);
+}
+
+TEST_F(
+    DefaultTableEntryComparerTests,
+    greaterThan_BothTeamsHaveSamePointsAndSameGoalDifferenceAndFirstTeamGreaterScoredGoals_RetrunsFalse)
+{
+  germanyTableEntry->addPoints(1);
+  englandTableEntry->addPoints(1);
+  germanyTableEntry->addGoalsScored(2);
+  germanyTableEntry->addGoalsAgainst(3);
+  englandTableEntry->addGoalsScored(1);
+  englandTableEntry->addGoalsAgainst(2);
+
+  bool result =
+      tableEntryComparer->greaterThan(*germanyTableEntry, *englandTableEntry);
+
+  ASSERT_TRUE(result);
 }
